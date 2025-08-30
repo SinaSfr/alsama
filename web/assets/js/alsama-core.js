eb56afda89c9f8af36748dfd6bfa56d05a38a155
kmg.d102 <kmg.d102@gmail.com>
2025-08-30 07:31:40 -0700

    search-engine updated to new version

--- web/assets/js/alsama-core.js
+++ web/assets/js/alsama-core.js
@@ -1,67 +1,69 @@
-function loadContentHomePage() {
-  loadSearchEngine("search-engine-en.bc", "search-box");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const isDesktop = window.innerWidth > 1024;
+  const requiredFiles = isDesktop
+    ? ["alsama.ui.min.css"]
+    : ["alsama-mob.ui.min.css"];
 
-async function loadSearchEngine(url, sectionload) {
-  try {
-    var xhrobj = new XMLHttpRequest();
-    xhrobj.open("GET", url);
-    xhrobj.send();
+  function checkAllResourcesLoaded() {
+    const resources = performance.getEntriesByType("resource");
+    const loadedFiles = resources
+      .map((res) => res.name.split("/").pop())
+      .filter((name) => requiredFiles.includes(name));
 
-    xhrobj.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        var container = document.getElementById(sectionload);
-        container.innerHTML = xhrobj.responseText;
+    return requiredFiles.every((file) => loadedFiles.includes(file));
+  }
 
-        var scripts = container.getElementsByTagName("script");
-        for (var i = 0; i < scripts.length; i++) {
-          var scriptTag = document.createElement("script");
-          if (scripts[i].src) {
-            scriptTag.src = scripts[i].src;
-            scriptTag.async = false;
-          } else {
-            scriptTag.text = scripts[i].textContent;
-          }
-          document.head
-            .appendChild(scriptTag)
-            .parentNode.removeChild(scriptTag);
-        }
-        const pathnamehome = window.location.pathname;
-        if (pathnamehome) {
-          if (pathnamehome == "/hotel") {
-            sessionStorage.setItem("pageName", "hotel");
-            $("#Hotel").click(function () {
-              $("#flight-type-items").hide();
-              $(".nav-module").each(function () {
-                var checknav = $(this).attr("data-nav");
-                if (checknav == "hotel") {
-                  $(this).addClass("nav-module-selected");
-                } else {
-                  $(this).removeClass("nav-module-selected");
-                }
-              });
-              LoadHotel();
-            });
-          }
-        } else if (pathnamehome == "/tour") {
-          sessionStorage.setItem("pageName", "tour");
-          $("#Tour").click(function () {
-            $("#flight-type-items").hide();
-            $(".nav-module").each(function () {
-              var checknav = $(this).attr("data-nav");
-              if (checknav == "tour") {
-                $(this).addClass("nav-module-selected");
-              } else {
-                $(this).removeClass("nav-module-selected");
+  if (document.getElementById("search-box")) {
+    function fetchEngine() {
+      try {
+        const xhrobj = new XMLHttpRequest();
+        xhrobj.open("GET", "test-engine.bc");
+        xhrobj.send();
+
+        xhrobj.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            const container = document.getElementById("search-box");
+            container.innerHTML = xhrobj.responseText;
+
+            [".Basis_Date.end_date", ".Basis_Date.start_date"].forEach(
+              (selector) => {
+                const dateInputs = document.querySelectorAll(selector);
+                dateInputs.forEach((input) => {
+                  input.placeholder = "";
+                });
               }
-            });
-            LoadTour();
-          });
-        }
+            );
+
+            const scripts = container.getElementsByTagName("script");
+            for (let i = 0; i < scripts.length; i++) {
+              const scriptTag = document.createElement("script");
+              if (scripts[i].src) {
+                scriptTag.src = scripts[i].src;
+                scriptTag.async = false;
+              } else {
+                scriptTag.text = scripts[i].textContent;
+              }
+              document.head
+                .appendChild(scriptTag)
+                .parentNode.removeChild(scriptTag);
+            }
+          }
+        };
+      } catch (error) {
+        console.error("مشکلی پیش آمده است. لطفا صبور باشید", error);
       }
-    };
-  } catch (error) {}
-}
+    }
+
+    function waitForFiles() {
+      if (checkAllResourcesLoaded()) {
+        fetchEngine();
+      } else {
+        setTimeout(waitForFiles, 500);
+      }
+    }
+    waitForFiles();
+  }
+});
 
 const headerMenu = document.querySelector(".header-menu");
 const headerMenuClose = document.querySelector(".header-menu-close");
@@ -159,41 +161,35 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-    const FetchPageNumPrev = async (dataPageNum) => {
-      const fetchContentArticle = document.querySelector(
-        ".fetch-content-article"
-      );
-      const cmsQuery = fetchContentArticle.getAttribute("data-catid");
-      const pagingResponse = await fetch(
-        `/article-load-items.bc?catid=${cmsQuery}&pagenum=${dataPageNum}`
-      );
-      const pagingData = await pagingResponse.text();
-      fetchContentArticle.innerHTML = pagingData;
-    };
+const FetchPageNumPrev = async (dataPageNum) => {
+  const fetchContentArticle = document.querySelector(".fetch-content-article");
+  const cmsQuery = fetchContentArticle.getAttribute("data-catid");
+  const pagingResponse = await fetch(
+    `/article-load-items.bc?catid=${cmsQuery}&pagenum=${dataPageNum}`
+  );
+  const pagingData = await pagingResponse.text();
+  fetchContentArticle.innerHTML = pagingData;
+};
 
-    const FetchPageNumNext = async (dataPageNum) => {
-      const fetchContentArticle = document.querySelector(
-        ".fetch-content-article"
-      );
-      const cmsQuery = fetchContentArticle.getAttribute("data-catid");
-      const pagingResponse = await fetch(
-        `/article-load-items.bc?catid=${cmsQuery}&pagenum=${dataPageNum}`
-      );
-      const pagingData = await pagingResponse.text();
-      fetchContentArticle.innerHTML = pagingData;
-    };
+const FetchPageNumNext = async (dataPageNum) => {
+  const fetchContentArticle = document.querySelector(".fetch-content-article");
+  const cmsQuery = fetchContentArticle.getAttribute("data-catid");
+  const pagingResponse = await fetch(
+    `/article-load-items.bc?catid=${cmsQuery}&pagenum=${dataPageNum}`
+  );
+  const pagingData = await pagingResponse.text();
+  fetchContentArticle.innerHTML = pagingData;
+};
 
-    const FetchWithPageNum = async (dataPageNum) => {
-      const fetchContentArticle = document.querySelector(
-        ".fetch-content-article"
-      );
-      const cmsQuery = fetchContentArticle.getAttribute("data-catid");
-      const pagingResponse = await fetch(
-        `/article-load-items.bc?catid=${cmsQuery}&pagenum=${dataPageNum}`
-      );
-      const pagingData = await pagingResponse.text();
-      fetchContentArticle.innerHTML = pagingData;
-    };
+const FetchWithPageNum = async (dataPageNum) => {
+  const fetchContentArticle = document.querySelector(".fetch-content-article");
+  const cmsQuery = fetchContentArticle.getAttribute("data-catid");
+  const pagingResponse = await fetch(
+    `/article-load-items.bc?catid=${cmsQuery}&pagenum=${dataPageNum}`
+  );
+  const pagingData = await pagingResponse.text();
+  fetchContentArticle.innerHTML = pagingData;
+};
 
 document.addEventListener("DOMContentLoaded", function () {
   if (document.querySelector(".blue-dropdown")) {
